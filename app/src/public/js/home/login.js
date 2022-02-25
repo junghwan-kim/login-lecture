b28e971cf6d1f074623ad893fdc51d0d27e8b897
junghwan-kim <hairmetal@naver.com>
2022-02-25 20:13:36 +0900

    돔을 이용한 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('hello');
+const id = document.querySelector('#id');
+const pwd = document.querySelector('#pwd');
+const loginBtn = document.querySelector('button');
+
+loginBtn.addEventListener("click",login);
+
+function login(){
+    const req = {
+        id: id.value,
+        pwd: pwd.value
+    };
+}
