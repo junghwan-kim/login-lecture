0f95632219a2a57cbb4f2042eea56a5f49d5bb82
junghwan-kim <hairmetal@naver.com>
2022-02-25 20:44:07 +0900

    프런트 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,45 @@
 'use strict';
 
-const hello = (req, res) => {
-    res.render('home/index');
+const output = {
+    home : (req, res) => {
+        res.render('home/index');
+    },    
+    login : (req, res)=>{
+        res.render('home/login');
+    }
 };
 
-const login = (req, res)=>{
-    res.render('home/login');
+
+const users = {
+    id: ["test1","test2","test3"],
+    pwd: ["1234","1111 ","1456"],
+}
+
+
+const process = {
+    login : (req, res) =>{
+        const id = req.body.id;
+        const pwd = req.body.pwd;
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.pwd[idx] === pwd){
+                return res.json({
+                    success : true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg:"로그인에 실패했습니다",
+        })
+
+    }
 };
+
 
 module.exports = {
-    hello,
-    login
+    output,
+    process,
 }
