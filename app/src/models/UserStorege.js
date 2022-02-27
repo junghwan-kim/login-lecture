5c863fbdd0651a2a57bc746deba0d62a1b0ab2af
junghwan-kim <hairmetal@naver.com>
2022-02-27 15:33:44 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorege.js
+++ app/src/models/UserStorege.js
@@ -1,15 +1,24 @@
 'use strict';
 
+const fs = require('fs').promises;
+
 //모델 class화 > 은닉 #은 숨김처리
 class UserStorege {
-    static #users = {
-        id: ["test1","test2","test3"],
-        pwd: ["1234","1111 ","1456"],
-        name: ["홍길동","김길동","장길동"],
-    };
+    
+    static #getUserInfo(data, id){
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser, info) =>{
+            newUser[info] = users[info][idx];
+            return newUser;
+        },{});
+        return userInfo;    
+    }
+
 
     static getUsers(...fields){
-        const users = this.#users;
+        //const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -20,18 +29,17 @@ class UserStorege {
     }
 
     static getUserInfo(id){
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);
-        const userInfo = usersKeys.reduce((newUser, info) =>{
-            newUser[info] = users[info][idx];
-            return newUser;
-        },{});
-        return userInfo;
+        //const users = this.#users;
+
+        return fs.readFile('./src/databases/users.json')
+            .then((data) => {
+                 return this.#getUserInfo(data, id);
+            })
+            .catch(console.error);
+        
     }
 
-    static save(userInfo){
-        
+    static save(userInfo){        
         return {success: true};
     }
 }
